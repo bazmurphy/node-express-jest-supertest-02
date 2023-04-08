62cb4f453f1b74e42c783a19f5a0cabeb6a2d024
Baz Murphy <bazmurphy@gmail.com>
2023-04-08 11:40:42 +0100

    additional

--- routes/__test__/index.test.js
+++ routes/__test__/index.test.js
@@ -2,8 +2,29 @@ const app = require("../../app");
 const request = require("supertest");
 
 describe("register", () => {
-  it("returns status code 201 if firstName is passed", async () => {
-    const res = await request(app).post("/register").send({ firstName: "Baz" });
+
+  it("returns status code 201 if firstName and lastName are passed", async () => {
+    const res = await request(app).post("/register").send({ firstName: "Baz", lastName: "Murphy" });
     expect(res.statusCode).toEqual(201);
-  })
+    expect(res.body).toEqual({ success: true, message: "Successfully created", data: { firstName: "Baz", lastName: "Murphy" } })
+  });
+
+  it("returns status code 400 if firstName is missing", async () => {
+    const res = await request(app).post("/register").send({ lastName: "Murphy" });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ success: false, message: "You need to provide a firstName", data: null })
+  });
+
+  it("returns status code 400 if lastName is missing", async () => {
+    const res = await request(app).post("/register").send({ firstName: "Baz" });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ success: false, message: "You need to provide a lastName", data: null })
+  });
+
+  it("returns status code 400 if firstName and lastName are missing", async () => {
+    const res = await request(app).post("/register").send({});
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ success: false, message: "You need to provide a firstName and a lastName", data: null })
+  });
+
 })
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  res.status(200).send("Please use POST on the /register Route")
+})
+
 router.post("/register", (req, res, next) => {
-  if (!req.body.firstName) {
+
+  const firstName = req.body?.firstName;
+  console.log(firstName);
+  const lastName = req.body?.lastName;
+  console.log(lastName);
+
+  if (!firstName && !lastName) {
     // 400 = Bad Request
-    res.status(400).json("you need to pass a firstName");
+    res.status(400).json({ success: false, message: "You need to provide a firstName and a lastName", data: null });
     return;
   }
+
+  if (!firstName) {
+    // 400 = Bad Request
+    res.status(400).json({ success: false, message: "You need to provide a firstName", data: null });
+    return;
+  }
+
+  if (!lastName) {
+    // 400 = Bad Request
+    res.status(400).json({ success: false, message: "You need to provide a lastName", data: null });
+    return;
+  }
+
   // 201 = Created
-  res.sendStatus(201);
+  res.status(201).json({ success: true, message: "Successfully created", data: { firstName, lastName } });
 });
 
 module.exports = router;
