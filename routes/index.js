3cda5f0feded7df7424660d79640749c4dcf7724
Baz Murphy <bazmurphy@gmail.com>
2023-04-08 11:08:45 +0100

    added vscode requests

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 router.post("/register", (req, res, next) => {
   if (!req.body.firstName) {
+    // 400 = Bad Request
     res.status(400).json("you need to pass a firstName");
     return;
   }
+  // 201 = Created
   res.sendStatus(201);
 });
 
